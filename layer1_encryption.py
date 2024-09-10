3c3b3fad2ad3db8e880a8c16a896d0d358013fbb
Pranav Kumar <kumarpranav3206@gmail.com>
2024-09-11 01:42:41 +0530

    encrypt decrypt utf-8

--- layer1_encryption.py
+++ layer1_encryption.py
@@ -2,6 +2,7 @@ from Crypto.Cipher import AES
 import base64
 import os
 
+
 # Planning on doing 128 bit AES key
 # Note - pycryto wasn't working ( buffer errors ) so pycrytodome added
 
@@ -16,4 +17,21 @@ class Encryption:
         # Padding messages so that they are multiple of 16
 
 
+    # encryption and decryption code 
+
+
+    def encrypt(self, message):
+        message = self.pad(message)
+        #Padding before encrypting
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        encrypted_message = base64.b64encode(cipher.encrypt(message.encode('utf-8')))
+        # Following utf-8 as of now
+        return encrypted_message
+
+    def decrypt(self, encrypted_message):
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        decrypted_message = cipher.decrypt(base64.b64decode(encrypted_message)).decode('utf-8').rstrip()
+        return decrypted_message
+
+    
 
